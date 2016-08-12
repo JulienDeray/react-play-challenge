98520a4b35143a0ed2c488a816efd7937006a2b4
Julien Déray <julien@deray.fr>
2016-08-12 17:15:48 +0100

    sets up the page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,39 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
 
-  setTimeout(() => console.log("ES6 !!!"), 1500);
-}
+const ArticlesBox = React.createClass({
+    render() {
+        return (
+            <div className='split-left'>
+                <h1>Articles :</h1>
+            </div>
+        );
+    }
+});
+
+const NewArticleBox = React.createClass({
+    render() {
+        return (
+            <div className='split-right'>
+                <h1>New article :</h1>
+            </div>
+        );
+    }
+});
+
+const TopLevelBox = React.createClass({
+    render() {
+        const height = $(window).height();
+        const width = $(window).width();
+
+        return (
+           <div style={{ height, width }}>
+               <ArticlesBox />
+               <NewArticleBox />
+           </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <TopLevelBox />,
+    document.getElementById('content')
+);
