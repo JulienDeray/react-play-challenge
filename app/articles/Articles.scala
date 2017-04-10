ccef57eaaeb0cc6f7bdf812e9ed3b817f11c1259
HybridLizard <dawid@hexorder.com>
2017-04-10 17:46:47 +0200

    Basic filtering and styling
    Changes:
    
    - Added basic filtering by title and clearing typed phrase
    - Added basic styling with simple responsivness
    - Added several more hardcoded articles

--- app/articles/Articles.scala
+++ app/articles/Articles.scala
@@ -16,6 +16,7 @@ object Articles {
 
   implicit val articleFormatter = Json.format[Article]
 
+  // NOTE: added bunch of hardcoded articles for easier checking of implemented features
   private var articles = List(
     Article(
       "First article",
@@ -26,6 +27,56 @@ object Articles {
       "Second awesome blog post",
       "Miss Catapulte",
       """You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man."""
+    ),
+    Article(
+      "Three",
+      "Darth Vader",
+      """I am your father!"""
+    ),
+    Article(
+      "Four",
+      "Luke Skywalker",
+      """Noooooo!"""
+    ),
+    Article(
+      "Five",
+      "John Nada",
+      """I have come here to chew bubblegum and kick ass... and I'm all out of bubblegum."""
+    ),
+    Article(
+      "Six",
+      "Leonidas",
+      """This is Sparta!"""
+    ),
+    Article(
+      "Seven",
+      "Braveheart",
+      """They may take our lives, but they'll never take our freedom!"""
+    ),
+    Article(
+      "Eight",
+      "Maximus",
+      """My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next."""
+    ),
+    Article(
+      "Nine",
+      "Han Solo",
+      """Chewie, we're home."""
+    ),
+    Article(
+      "Ten",
+      "Harry Callahan",
+      """Go ahead, make my day."""
+    ),
+    Article(
+      "Eleven",
+      "Leia",
+      """Help me, Obi-Wan Kenobi. You're my only hope."""
+    ),
+    Article(
+      "Twelve",
+      "Terminator",
+      """Hasta la vista, baby."""
     )
   )
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,7 @@
+// NOTE: currently it is advised to use ES6 classes as a replacement for older createClass.
+// Babel is used anyway so it would not be a problem. Set is as a TODO for future.
+// Also adding a build-step (Webpack) would be a good choice as app grows to split this file into smaller ones
+
 
 const NewArticleForm = React.createClass({
     getInitialState() {
@@ -20,10 +24,10 @@ const NewArticleForm = React.createClass({
     },
     render() {
         return (
-           <div>
-               <p>Title : <input type='text' value={this.state.title} onChange={this.handleTitleChange} /></p>
-               <p>Author : <input type='text' value={this.state.author} onChange={this.handleAuthorChange} /></p>
-               <p>Content : <textarea value={this.state.content} onChange={this.handleContentChange} /></p>
+           <div className="rpc-form">
+               <p><label>Title:</label> <input type='text' value={this.state.title} onChange={this.handleTitleChange} /></p>
+               <p><label>Author:</label> <input type='text' value={this.state.author} onChange={this.handleAuthorChange} /></p>
+               <p><label>Content:</label> <textarea value={this.state.content} onChange={this.handleContentChange} /></p>
                <button onClick={this.postNewArticle}>Publish</button>
            </div>
         );
@@ -33,7 +37,7 @@ const NewArticleForm = React.createClass({
 const Article = React.createClass({
     render() {
         return (
-           <div>
+           <div className="rpc-article">
                <h3>{this.props.title}</h3>
                <p>Author: {this.props.author}</p>
                <p>{this.props.content}</p>
@@ -56,9 +60,56 @@ const ArticlesList = React.createClass({
                 : <h2>No articles</h2>;
 
         return (
-            <div>
+            <div className="rpc-articles-list">
                 { list }
             </div>
+        );
+    }
+});
+
+/**
+ * Filter articles by a simple title partial match checking. TODO: Also the filter can be cleared.
+ */
+const FilterByTitle = React.createClass({
+    getInitialState() {
+        return {filterText: ""}
+    },
+    clearValue(event) {
+        this.props.clearFilter(event);
+
+        this.setState((prevState) => ({
+            filterText: ""
+        }));
+    },
+    updateValue(event) {
+        var val = event.target.value;
+
+        this.props.filter(event);
+
+        this.setState((prevState) => ({
+            filterText: val
+        }));
+    },
+    render() {
+        return (
+            <div className="rpc-article-filter">
+                 <input type="text" placeholder="Filter by title" value={this.state.filterText} onChange={this.updateValue} />
+                 <button onClick={this.clearValue}>Clear</button>
+            </div>
+        );
+    }
+});
+
+/**
+ * TODO: Filter articles by simple title partial match checking. Also filter can be cleared.
+ */
+const ContentToggler = React.createClass({
+    render() {
+        return (
+            <label className="rpc-filter-clear">
+                <input type="checkbox" onChange={this.props.toggleContent} />
+                <span>Toggle content</span>
+            </label>
         );
     }
 });
@@ -66,8 +117,12 @@ const ArticlesList = React.createClass({
 const ArticlesBox = React.createClass({
     render() {
         return (
-            <div className='split-left'>
-                <h1>Articles :</h1>
+            <div className='rpc-split-left'>
+                <h1>Articles</h1>
+                <div className="rpc-articles-settings">
+                    <FilterByTitle articles={this.props.articles} filter={this.props.filter} clearFilter={this.props.clearFilter} />
+                    <ContentToggler toggleContent={this.props.toggleContent} />
+                </div>
                 <ArticlesList articles={this.props.articles} />
                 <hr />
                 <h4>Articles are sponsored by <a href='http://slipsum.com'>SAMUEL L. IPSUM</a></h4>
@@ -79,8 +134,8 @@ const ArticlesBox = React.createClass({
 const NewArticleBox = React.createClass({
     render() {
         return (
-            <div className='split-right'>
-                <h1>New article :</h1>
+            <div className='rpc-split-right'>
+                <h1>New article</h1>
                 <NewArticleForm postNewArticle={this.props.postNewArticle} />
             </div>
         );
@@ -92,7 +147,9 @@ const TopLevelBox = React.createClass({
         this.getArticlesFromBackend()
     },
     getInitialState() {
-        return { articles: [] };
+        // NOTE: added cache here to not make additional ajax call after filtering to rest articles list
+        // More sofisticated mechanism could be used here in real scale application
+        return { articles: [], articlesCache: [] };
     },
     getArticlesFromBackend() {
         $.ajax({
@@ -100,7 +157,10 @@ const TopLevelBox = React.createClass({
             dataType: 'json',
             type: 'GET',
             success: function(articles) {
-                this.setState({ articles });
+                this.setState((prevState) => ({
+                    articles: articles,
+                    articlesCache: articles
+                }));
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -115,22 +175,56 @@ const TopLevelBox = React.createClass({
             data: JSON.stringify(newArticle),
             success: function() {
                 successCallback();
-                const articles = this.state.articles;
-                this.setState({ articles: articles.concat([newArticle]) });
+                const articles = this.state.articlesCache;
+                this.setState((prevState) => ({
+                    articles: articles.concat([newArticle]),
+                    articlesCache: articles.concat([newArticle])
+                }));
+                //this.setState({ articlesCache: articles.concat([newArticle]) });
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
             }.bind(this)
         });
     },
-    render() {
-        const height = $(window).height();
-        const width = $(window).width();
+    /**
+     * Simple partial match of title and typed phrase is used for filtering.
+     */
+    filterArticlesByTitle(event) {
+        const articles = this.state.articlesCache;
+        var title = event.target.value;
 
+        this.setState((prevState) => ({
+            articles: articles.filter((article) => {
+                return (article.title.indexOf(title) !== -1)
+            })
+        }));
+    },
+    /**
+     * Just for user convenience a helper to clear typed filter text.
+     */
+    clearTitleFilter(event) {
+        const articles = this.state.articlesCache;
+        var toggle = event.target.value;
+
+        this.setState((prevState) => ({
+            articles: articles
+        }));
+    },
+    /**
+     * TODO: Another convenience utility for easier scanning only article headers.
+     */
+    toggleContent(event) {
+        console.log("TODO: toggle change: ");
+    },
+    render() {
+        // NOTE: removed width & height calculations,
+        // as it is not needed anymore,
+        // moved to CSS and added basic responsivness
         return (
-           <div style={{ height, width }}>
-               <ArticlesBox articles={this.state.articles} />
+           <div className="rpc-app">
                <NewArticleBox postNewArticle={this.postNewArticle} />
+               <ArticlesBox articles={this.state.articles} filter={this.filterArticlesByTitle} clearFilter={this.clearTitleFilter} />
            </div>
         );
     }
